702244cb3736ff0f93956233f72bbdb83cc06a53
LucasSpinelli003 <rm97757@fiap.com.br>
2023-10-20 18:06:48 -0300

    Initial commit, ts/react timer

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./Components/Button";
+
+export function App() {
+
+  return (
+    <>
+      <Button color="primary"/>
+      <Button color="secondary"/>
+      <Button color="success"/>
+      <Button color="danger"/>
+      <Button />
+    </>
+  )
+}
+
+
